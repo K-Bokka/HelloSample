227cc581302bddee355a3e8e8c589325608cf846
Akira Yamamoto <ak-yamamoto@k-bokka.com>
2020-01-04 19:38:46 +0900

    Add listener

--- app/src/main/java/com/kbokka/android/hellosample/MainActivity.kt
+++ app/src/main/java/com/kbokka/android/hellosample/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.kbokka.android.hellosample
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    private inner class HelloListener : View.OnClickListener {
+        @SuppressLint("SetTextI18n")
+        override fun onClick(view: View) {
+            val input = findViewById<EditText>(R.id.etName)
+            val output = findViewById<TextView>(R.id.tvOutput)
+
+            val inputString = input.text.toString()
+
+            output.text = inputString + "さん、こんにちは！"
+        }
     }
 }
